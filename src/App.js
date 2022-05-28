f3c3fead6c1f4bddad2a05ceba5691326c06b1f0
keitha13 <keitha9452@gmail.com>
2022-05-28 13:13:07 +0900

    add categories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from "react";
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch } from 'react-router-dom';
 import Movies from './components/Movies';
 import Admin from './components/Admin';
 import Home from './components/Home';
+import Categories from "./components/Categories";
 
 
 export default function App() {
@@ -26,6 +27,9 @@ export default function App() {
                 <Link to="/movies">Movies</Link>
               </li>
               <li className='list-group-item'>
+                <Link to="/by-category">Categories</Link>
+              </li>
+              <li className='list-group-item'>
                 <Link to="/admin">Manage Catalogue</Link>
               </li>
             </ul>
@@ -33,9 +37,17 @@ export default function App() {
         </div>
         <div className='col-md-10'>
           <Switch>
+            <Route path="/movies/:id">
+              <Movie />
+            </Route>
             <Route path="/movies">
               <Movies />
             </Route>
+            <Route exact path="/by-category">
+              <CategoryPage />
+            </Route>
+            <Route exact path="/by-category/drama" render={(props) => <Categories {...props} title={`Drama`} />}/>
+            <Route exact path="/by-category/comedy" render={(props) => <Categories {...props} title={`Comedy`} />}/>
             <Route path="/admin">
               <Admin />
             </Route>
@@ -48,4 +60,20 @@ export default function App() {
     </div>
     </Router>
   );
+}
+
+function Movie(){
+  let { id } = useParams();
+  return <h2>Movie id {id}</h2>
+}
+
+function CategoryPage() {
+  let {path, url} = useRouteMatch();
+  return (
+    <div>
+      <h2>Categories</h2>
+      <li><Link to={`${path}/comedy`}>Comedy</Link></li>
+      <li><Link to={`${path}/drama`}>Drama</Link></li>
+    </div>
+  )
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,32 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
+import { Link } from "react-router-dom";
 
 export default class Movies extends Component {
+
+  state = { movies: []}
+
+  componentDidMount() {
+    this.setState({
+      movies: [
+        {id: 1, title: "movie one", runtime: 142},
+        {id: 2, title: "movie twe", runtime: 102},
+        {id: 3, title: "movie three", runtime: 98},
+      ]
+    })
+  }
+
   render() {
     return (
-      <h2>choose a movie</h2>
-    )
+      <Fragment>
+        <h2>choose a movie</h2>
+        <ul>
+          {this.state.movies.map((m) => (
+            <li key={m.id}>
+              <Link to={`/movies/${m.id}`}>{m.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
   }
 }
