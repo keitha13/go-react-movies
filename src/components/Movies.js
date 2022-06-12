d530c530d36b62a199fc9115ed271860d749f240
keitha13 <keitha9452@gmail.com>
2022-06-12 09:28:43 +0900

    fix component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,30 +3,56 @@ import { Link } from "react-router-dom";
 
 export default class Movies extends Component {
 
-  state = { movies: []}
+  state = {
+    movies: [],
+    isLoaded: false,
+    error: null,
+  };
 
   componentDidMount() {
-    this.setState({
-      movies: [
-        {id: 1, title: "movie one", runtime: 142},
-        {id: 2, title: "movie twe", runtime: 102},
-        {id: 3, title: "movie three", runtime: 98},
-      ]
+    fetch("http://localhost:4000/v1/movies")
+    .then((response) => {
+      console.log("status code is", response.status);
+      if (response.status !== "200") {
+        let err = Error;
+        err.message = "Invalid response code: " + response.status;
+        this.setState({error: err});
+      }
+      return response.json();
     })
+    .then((json) => {
+      this.setState({
+        movies: json.movies,
+        isLoaded: true,
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      });
+    });
   }
 
   render() {
-    return (
-      <Fragment>
-        <h2>choose a movie</h2>
-        <ul>
-          {this.state.movies.map((m) => (
-            <li key={m.id}>
-              <Link to={`/movies/${m.id}`}>{m.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </Fragment>
-    );
+    const { movies, isLoaded, error } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded){
+      return<p>Loading...</p>
+    } else {
+      return (
+        <Fragment>
+          <h2>choose a movie</h2>
+          <ul>
+            {movies.map((m) => (
+              <li key={m.id}>
+                <Link to={`/movies/${m.id}`}>{m.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      );
+    }
   }
 }
